fe5c3d7d23149ef93eccba94b5266468195342fa
ATARO-QQQ <150531492+ATARO-QQQ@users.noreply.github.com>
2026-07-14 08:33:41 +0900

    Add Tailwind CSS configuration for theme colors and fonts

---
+++ script/tailwind.js
@@ -0,0 +1,16 @@
+        tailwind.config = {
+            theme: {
+                extend: {
+                    colors: {
+                        brand: {
+                            light: '#e0f2fe',
+                            DEFAULT: '#0284c7',
+                            dark: '#0369a1',
+                        }
+                    },
+                    fontFamily: {
+                        sans: ['"Inter"', '"Noto Sans JP"', 'sans-serif'],
+                    }
+                }
+            }
+        }
